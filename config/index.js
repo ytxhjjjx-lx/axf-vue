fc2bd6e121a64373a069ca3383f5e0cd81c4f1e7
lx's repository <31095285+ytxhjjjx-lx@users.noreply.github.com>
2018-07-02 15:17:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -14,6 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
+    // 端口号
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -48,6 +49,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
+    // 打包后的文件路径
     assetsPublicPath: '/',
 
     /**
